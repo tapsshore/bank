1db17bf0cc25b6470e7dfffd22b5b1e6961ecf01
tapsshore <tapsshore@gmail.com>
2021-06-28 00:14:49 +0200

    added swaggerconfig

---
+++ src/main/java/co/za/wonderlabz/bank/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package co.za.wonderlabz.bank.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("za.co.wonderlabz.bank.api.account"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(new ApiInfoBuilder()
+                        .version("1.0")
+                        .title("")
+                        .contact(new Contact("Tapiwanashe Shoshore", "https://www.wonderlabz.co.za/",
+                                "tapsshore@gmail.com"))
+                        .description("Documentation for Banking  API v1.0")
+                        .build());
+    }
+
+
+}
